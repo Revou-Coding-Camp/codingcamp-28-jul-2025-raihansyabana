6a5cdb16e964746c969af86fae1fc6cf9aa10293
Raihan Syabana <raihansyabana@gmail.com>
2025-08-02 08:00:37 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -23,16 +23,16 @@ function validateForm() {
 
 // Function to display a welcome message
 // This function prompts the user for their name and updates the welcome speech element
-// function welcomeMessage() {
-//     // Prompt the user for their name and display it in the welcome speech element
-//     const popup = prompt('Please enter your name:');
+function welcomeMessage() {
+    // Prompt the user for their name and display it in the welcome speech element
+    const popup = prompt('Please enter your name:');
 
-//     // If the user provides a name, update the welcome speech element
-//     if (popup) {
-//         // Get the welcome speech element and set its text content to the user's name
-//         const welcomeElement = document.getElementById('welcome-speech');
+    // If the user provides a name, update the welcome speech element
+    if (popup) {
+        // Get the welcome speech element and set its text content to the user's name
+        const welcomeElement = document.getElementById('welcome-speech');
 
-//         // Update the text content of the welcome speech element with the user's name
-//         welcomeElement.textContent = popup;
-//     }
-// }
+        // Update the text content of the welcome speech element with the user's name
+        welcomeElement.textContent = popup;
+    }
+}
